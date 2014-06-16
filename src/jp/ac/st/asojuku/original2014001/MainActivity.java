276d7dfbe734fca84a478fbe4a5773050081a6cb
student <student@PC111S32.fgakusei.local>
2014-06-16 11:19:28 +0900

    nmnm

--- src/jp/ac/st/asojuku/original2014001/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014001/MainActivity.java
@@ -3,10 +3,10 @@ package jp.ac.st.asojuku.original2014001;
 import java.util.Random;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -31,12 +31,47 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		switch(v.getId()){ //どのボタンが押されたか判定
 		case R.id.btnOK: //btnが押された
 			//エディトテキストから入力内容を取り出す
-			EditText evt = (EditText)findViewById(R.id.edtName);
-			String inputdaikiti = etv.getText().toString();
+			//EditText evt = (EditText)findViewById(R.id.edtName);
+			//String inputdaikiti = etv.getText().toString();
 
 			//Randomクラスのインスタンスを作る
 			Random rnd = new Random();
+			// 0～から3の4パターンの数字をranに取得
+			int ran = rnd.nextInt(4);
 
+			//生成して代入用のIntentインスタンス変数を用意
+			Intent intent = null;
+			// ranの値によって処理をわける
+			switch(ran){
+			case 0:
+				//0なら大吉のページに飛ばす
+				//インテントのインスタンス生成
+				intent = new Intent(MainActivity.this,daikiti.class);
+				//次画面のアクティビティ起動
+				startActivity(intent);
+				break;
+			case 1:
+			    //1なら中吉のページに飛ばす
+			    //インテントのインスタンス生成
+		        intent = new Intent(MainActivity.this,tyuukiti.class);
+			    //次画面のアクティビティ起動
+			    startActivity(intent);
+			break;
+		    case 2:
+		       //2なら凶のページに飛ばす
+		       //インテントのインスタンス生成
+	           intent = new Intent(MainActivity.this,kyou.class);
+		       //次画面のアクティビティ起動
+		       startActivity(intent);
+		       break;
+	        case 3:
+	      //3なら大凶のページに飛ばす
+			//インテントのインスタンス生成
+			intent = new Intent(MainActivity.this,daikyou.class);
+			//次画面のアクティビティ起動
+			startActivity(intent);
+			break;
+			}
 
 		}
 
